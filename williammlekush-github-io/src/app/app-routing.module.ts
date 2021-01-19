04e93fa0b794f7c4464e464ee5c678081f955487
bassmonkey620 <bassmonkey620@users.noreply.github.com>
2021-01-18 23:22:30 -0500

    feat: setup module structure

--- williammlekush-github-io/src/app/app-routing.module.ts
+++ williammlekush-github-io/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'navigation', loadChildren: () => import('./navigation/navigation.module').then(m => m.NavigationModule) }, { path: 'gallery', loadChildren: () => import('./gallery/gallery.module').then(m => m.GalleryModule) }, { path: 'project', loadChildren: () => import('./project/project.module').then(m => m.ProjectModule) }, { path: 'resume', loadChildren: () => import('./resume/resume.module').then(m => m.ResumeModule) }, { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
